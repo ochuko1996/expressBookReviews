4dca092144eaa3a1725d38c38d6b0a672ec6c148
ochuko1996 <georgeochuko7@.com>
2024-05-15 12:53:29 -0400

    auth added

--- final_project/index.js
+++ final_project/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function asuth(req,res,next){
+app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
 if(req.session.authorization) {
     token = req.session.authorization['accessToken'];
